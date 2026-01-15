478ce203e7778f429bcea253b22985b19d3e58b1
Anastezia13 <totosha-950@mail.ru>
2026-01-15 13:51:45 +0300

    a

--- script.js
+++ script.js
@@ -4,9 +4,21 @@ const showButtonBrand = document.querySelector("#showBrand");
 
 const hideButtonBrand = document.querySelector("#hideBrand");
 
+function calculateRow() {
+    if (window.innerWidth >= 1440) {
+        return 4;
+    } else if (window.innerWidth >= 768) {
+        return 3;
+    }
+}
+
+// скрыть
 function hideLast() {
+    itemsPerRow = calculateRow();
+    const visible = itemsPerRow * 2;
+
     allItems.forEach(function (item, index) {
-        if (index >= 8) {
+        if (index >= visible) {
             item.classList.add("close");
         } else {
             item.classList.remove("close");
@@ -17,6 +29,7 @@ function hideLast() {
     showButtonBrand.style.display = "block";
 }
 
+// показать
 function showAll() {
     allItems.forEach(function (item) {
         item.classList.remove("close");
@@ -32,31 +45,14 @@ window.addEventListener("load", function () {
     hideButtonBrand.addEventListener("click", hideLast);
 });
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+// ресайз окна
+function resize() {
+    if (hideButtonBrand.style.display !== "block") {
+        hideLast();
+    }
+}
 
-// const showButtonAbout = document.querySelector("#showAbout");
-
-// const hideButtonAbout = document.querySelector("#hideAbout");
-
-// function hideAbout() {
-
-// }
-
-// function showAbout() {
-
-// }
+window.addEventListener("resize", resize);
 
 //
 //
@@ -68,18 +64,14 @@ window.addEventListener("load", function () {
 //
 //
 //
-//
 
+// свайпер
 
+const listItems = document.querySelectorAll(".brand__item");
 
+const swiperWrapper = document.querySelector(".swiper-wrapper");
 
-const listItems = document.querySelectorAll('.brand__item');
-
-const swiperWrapper = document.querySelector('.swiper-wrapper');
-
-
-
-listItems.forEach(item => {
+listItems.forEach((item) => {
     swiperWrapper.innerHTML += `
         <div class="swiper-slide">
             ${item.innerHTML}
@@ -87,23 +79,59 @@ listItems.forEach(item => {
     `;
 });
 
-
-
 const swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-
     },
 
     spaceBetween: 16,
-    slidesPerView: 1.2, 
-
-
-
-
-
-
-
+    slidesPerView: 1.2,
 });
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// цены
+
+// prettier-ignore
+
+const prices = [{
+    service: "Диагностика",
+    price: "Бесплатно",
+    time: "30 мин",
+},{
+    service: "Замена дисплея",
+    price: "1 000 ₽",
+    time: "30-120 мин",
+},{
+    service: "Замена полифонического динамика",
+    price: "1 000 ₽",
+    time: "30-120 мин",
+},{
+    service: "Тестирование с выдачей тех. заключения",
+    price: "1 000 ₽",
+    time: "30-120 мин",
+},{
+    service: "Замена программного обеспечения",
+    price: "1 000 ₽",
+    time: "30-120 мин",
+},];
+
+function renderPrise() {
+    const div = document.createElement("div");
+    div.className = "price__tab";
+
+    for (let i = 0; i < prices.length; i++) {
+        const priseItem = prises[i];
+
+        div.insertAdjacentHTML("beforeend", ``);
+    }
+}
