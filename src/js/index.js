fbcb75ab8df2942d4f278162d6d8b2ae7d6c4d44
Anastezia13 <totosha-950@mail.ru>
2026-02-06 02:18:00 +0300

    a

--- src/js/index.js
+++ src/js/index.js
@@ -1,209 +1,145 @@
-
-
-
 import '../scss/style.scss'
 
 // бренды
 
-let itemsPerRow = 4;
+let itemsPerRow = 4
 
+const allItems = document.querySelectorAll('.brand__item')
 
-const allItems = document.querySelectorAll(".brand__item");
+const showButtonBrand = document.querySelector('#showBrand')
 
-const showButtonBrand = document.querySelector("#showBrand");
-
-const hideButtonBrand = document.querySelector("#hideBrand");
+const hideButtonBrand = document.querySelector('#hideBrand')
 
 function calculateRow() {
     if (window.innerWidth >= 1440) {
-        return 4;
+        return 4
     } else if (window.innerWidth >= 768) {
-        return 3;
+        return 3
     }
 }
 
 // скрыть
 
-
 function hideLast() {
-    itemsPerRow = calculateRow();
-    const visible = itemsPerRow * 2;
+    itemsPerRow = calculateRow()
+    const visible = itemsPerRow * 2
 
     allItems.forEach(function (item, index) {
         if (index >= visible) {
-            item.classList.add("close");
+            item.classList.add('close')
         } else {
-            item.classList.remove("close");
+            item.classList.remove('close')
         }
-    });
+    })
 
-    hideButtonBrand.style.display = "none";
-    showButtonBrand.style.display = "block";
+    hideButtonBrand.style.display = 'none'
+    showButtonBrand.style.display = 'block'
 }
 
 // показать
 function showAll() {
     allItems.forEach(function (item) {
-        item.classList.remove("close");
-    });
+        item.classList.remove('close')
+    })
 
-    showButtonBrand.style.display = "none";
-    hideButtonBrand.style.display = "block";
+    showButtonBrand.style.display = 'none'
+    hideButtonBrand.style.display = 'block'
 }
 
-window.addEventListener("load", function () {
-    hideLast();
-    showButtonBrand.addEventListener("click", showAll);
-    hideButtonBrand.addEventListener("click", hideLast);
-});
+window.addEventListener('load', function () {
+    hideLast()
+    showButtonBrand.addEventListener('click', showAll)
+    hideButtonBrand.addEventListener('click', hideLast)
+})
 
 // ресайз окна
 function resize() {
-    if (hideButtonBrand.style.display !== "block") {
-        hideLast();
+    if (hideButtonBrand.style.display !== 'block') {
+        hideLast()
     }
 }
 
-window.addEventListener("resize", resize);
+window.addEventListener('resize', resize)
 
 // свайпер
 
-const listItems = document.querySelectorAll(".brand__item");
+const listItems = document.querySelectorAll('.brand__item')
 
-const swiperWrapper = document.querySelector(".swiper-wrapper");
+const swiperWrapper = document.querySelector('.swiper-wrapper')
 
 listItems.forEach((item) => {
     swiperWrapper.innerHTML += `
         <div class="swiper-slide">
             ${item.innerHTML}
         </div>
-    `;
-});
+    `
+})
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper', {
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+        el: '.swiper-pagination',
+        clickable: true
     },
 
     spaceBetween: 16,
-    slidesPerView: 'auto',
- });
-
-
-
-
-
-
-
+    slidesPerView: 'auto'
+})
 
 // типы
 
-
-
-
-
-
-
-
-
-
-
-
-
 // цены
 
-
 const services = [
-    ["Диагностика", "Бесплатно", "30 мин"],
-    ["Замена дисплея", "1 000 ₽", "30–120 мин"],
-    ["Замена полифонического динамика", "1 000 ₽", "30–120 мин"],
-    ["Тестирование с выдачей технического заключения", "1 000 ₽", "30–120 мин"],
-    ["Замена программного обеспечения", "1 000 ₽", "30–120 мин"]
-];
-
-
+    ['Диагностика', 'Бесплатно', '30 мин'],
+    ['Замена дисплея', '1 000 ₽', '30–120 мин'],
+    ['Замена полифонического динамика', '1 000 ₽', '30–120 мин'],
+    ['Тестирование с выдачей технического заключения', '1 000 ₽', '30–120 мин'],
+    ['Замена программного обеспечения', '1 000 ₽', '30–120 мин']
+]
 
 document.querySelector('.price__tab').innerHTML = `
     <table class="price-table">
         <tr><th>Услуга</th><th>Цена</th><th>Срок</th><th></th></tr>
-        ${services.map(s => `
+        ${services
+            .map(
+                (s) => `
             <tr>
                 <td>${s[0]}</td>
                 <td>${s[1]}</td>
                 <td>${s[2]}</td>
                 <td><button class="order-btn">ЗАКАЗАТЬ ></button></td>
             </tr>
-        `).join('')}
+        `
+            )
+            .join('')}
     </table>
-`;
-
-
-
-
-
+`
 
 // боковое меню
 
-
 function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('sidebar--open');
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.classList.toggle('sidebar--open')
 }
 
-document.querySelector('.btn-burger').addEventListener('click', toggleSidebar);
-document.querySelector('.btn-exit').addEventListener('click', toggleSidebar);
+document.querySelector('.btn-burger').addEventListener('click', toggleSidebar)
+document.querySelector('.btn-exit').addEventListener('click', toggleSidebar)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// заказать звонок 
+// заказать звонок
 document.querySelector('.btn-call').addEventListener('click', () => {
-    // Открываем форму
-    document.querySelector('.call').classList.add('active');
-    // Показываем оверлей
-    document.body.classList.add('call-open');
-});
+    document.querySelector('.call').classList.add('active')
+    document.body.classList.add('call-open')
+})
 
 document.getElementById('cls-call').addEventListener('click', () => {
-    // Закрываем форму
-    document.querySelector('.call').classList.remove('active');
-    // Скрываем оверлей
-    document.body.classList.remove('call-open');
-});
+    document.querySelector('.call').classList.remove('active')
+    document.body.classList.remove('call-open')
+})
 
-// Также можно закрывать по клику на оверлей
 document.querySelector('.overlay').addEventListener('click', () => {
-    document.querySelector('.call').classList.remove('active');
-    document.body.classList.remove('call-open');
-});
+    document.querySelector('.call').classList.remove('active')
+    document.body.classList.remove('call-open')
+})
